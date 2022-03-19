6c9edf84ab5acf070817fe421a0a5cf90d93d01e
hieumtran <hieutran_2023@depauw.edu>
2022-03-19 12:09:15 -0400

    Update pypuzzle

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# TenzerHackRacs
-Tenzer first hackathon
+# puzzlemania
+a puzzle game.
+
+v. 3.0: changed sounds, still many things to change in music.
+With m you can change the music, but for now there are just two.
+There is an issue: if you leave a tile on more tiles fixed, the
+tile will be lost... need to fix this.
+
+# Controls
+
+- Space: change puzzle
+
+-left mouse:move the tile
+
+- rightmouse: flip tile horizzontally
+
+- middle mouse up or down:flip tile vertically
+
+- "m" key to change the music
+
+- "s" to stop the music
+
+# Youtube gameplay
+
+
+https://youtu.be/nTyGLVHLGNY
+
+
+# Executable on itch.io
+
+
+https://formazione.itch.io/puzzle-mania-26
+
+# Fixed issues
+
+2.8
+
+- works on mac os (path issues fixed with pathlib)
+
+- music and new sounds added
---
+++ functions/synth.py
@@ -0,0 +1,93 @@
+import pygame
+import pygame.midi
+from time import sleep
+import sys
+from random import choice, randrange
+
+
+C = 74
+MAX = 127
+brief = .5
+
+count = 0
+def midi(note=[C], volume=MAX, length=brief):
+    global count
+
+    count += 1
+    if randrange(-1, 10) > 1:
+        # count < 4:
+        l = randrange(1, 50)
+        length = l / 30
+        for n in note:
+            midi_out.note_on(n, volume) # 74 is middle C, 127 is "how loud" - max is 127
+        sleep(brief)
+        for n in note: 
+            midi_out.note_off(n, volume)
+    else:
+        sleep(brief)
+        count = 0
+
+    # sleep(brief)
+
+
+#                init
+# =======================================
+GRAND_PIANO = 0
+CHURCH_ORGAN = 0
+instrument = GRAND_PIANO
+pygame.init()
+pygame.midi.init()
+port = pygame.midi.get_default_output_id()
+midi_out = pygame.midi.Output(port, 0)
+midi_out.set_instrument(instrument)
+print ("using output_id :%s:" % port)
+# =======================================
+
+def exit():
+    global midi_out, music
+
+    music = 0
+    del midi_out
+    pygame.midi.quit()
+    pygame.quit()
+    sys.exit()
+
+def casual():
+    a = choice([C, CM, D, E, Dm, F, G, A, FM, Em])
+    a[0] = a[0] + randrange(-1, 1) * 12
+    return a
+
+CM = [74, 78, 81]
+C = [62]
+Dm = [74, 76, 81]
+D = [76]
+E = [78]
+Em = [78, 81, 85]
+F = [79]
+G = [81]
+A = [83]
+FM = [72, 76, 79]
+screen = pygame.display.set_mode((400, 400))
+music = 1
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                exit()
+
+    if music:
+        x = []
+        for s in range(4):
+            a = casual()
+            x.append(a)
+            midi(a)
+            print(a)
+        for s in range(4):
+            midi(x[3 - s])
+            print(x[3 -s])
+        print("-----------")
+
+
+
