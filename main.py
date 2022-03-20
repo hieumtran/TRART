ab5e39a40019641fe0476be8c3d18984e2f59097
hieumtran <hieutran_2023@depauw.edu>
2022-03-20 02:26:59 -0400

    Update all

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import os, sys
 from slide_puzzle import SlidePuzzle
+from menu import menuUI
 
 
 
@@ -10,22 +11,39 @@ def main():
     pygame.display.set_caption("Slide Puzzle")
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     fpsclock = pygame.time.Clock()
-    program = SlidePuzzle((3,3), 100, 5)
     
+    w_screen, h_screen = pygame.display.get_surface().get_size() 
     
+    ui = True
+    program = SlidePuzzle((3,3), 160, 5, w_screen, h_screen)
+    menu_ui = menuUI('./ui_button/DeSwipe.png', './ui_button/start.png', './ui_button/quit.png',
+                     ['./ui_button/3x3.png', './ui_button/4x4.png', './ui_button/5x5.png'], './ui_button/back.png', w_screen, h_screen)
+    
+    # Condition of ui
+    cond = 0
+    prev_cond = 0
+    pos = 0, 0
     while True:
         dt = fpsclock.tick()/1000
         
         # screen.fill((1,1,1)) # Filling with missing pictures
         screen.fill([255, 255, 255])
         
-        program.draw(screen)
+        if cond < 2:
+            cond = menu_ui.draw(screen, cond, pos)
+            if prev_cond != cond:
+                pos = 0, 0
+                prev_cond == cond
+        elif cond >= 2:
+            program.draw(screen, fpsclock)
         pygame.display.flip()
         
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+        
+        if cond == -1:
+            sys.exit()
                 
         program.update(dt)
 
--- slide_puzzle.py
+++ slide_puzzle.py
@@ -1,12 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jul 13 18:44:06 2019
-@author: Manish
-"""
-
 import pygame, sys, os
 from random import shuffle
 import copy
+import time
 
 
 # gs - grid size
@@ -14,7 +9,8 @@ import copy
 # ms - margin size
 
 class SlidePuzzle:
-    def __init__(self, gs, ts, ms):
+    def __init__(self, gs, ts, ms, w_screen, h_screen):
+        # Initialize parameters
         self.gs, self.ts, self.ms = gs, ts, ms
         self.tiles_len = (gs[0]*gs[1]) - 1
         self.tiles = [(x,y) for x in range(gs[0]) for y in range(gs[1])]
@@ -22,12 +18,25 @@ class SlidePuzzle:
         self.tilespos = {(x,y):(x*(ts+ms)+ms,y*(ts+ms)+ms) for y in range(gs[1]) for x in range(gs[0])}   
         self.font = pygame.font.Font(None, 120)    
         self.w, self.h = gs[0]*(ts+ms)+ms, gs[1]*(ts+ms)+ms
+        
+        # Picture loader
         self.pic = pygame.image.load('./building/peeler.jpg')
         self.pic = pygame.transform.scale(self.pic, (self.w, self.h))
+        
+        # Button
+        self.back_button = pygame.image.load('./ui_button/back_button.png')
+        self.back_button = pygame.transform.scale(self.back_button, (self.w/4, self.h/4))
+        self.retry = pygame.image.load('./ui_button/retry.png')
+        self.retry = pygame.transform.scale(self.retry, (self.w/4, self.h/4))
+        
+        # Game condition
         self.finish = 0
         
+        # Windows screen 
+        self.w_screen = w_screen
+        self.h_screen = h_screen
+        
         # Scaling factor
-        w_screen, h_screen = pygame.display.get_surface().get_size() 
         self.w_adjust = (w_screen - self.ts*self.gs[0] - self.ms*(self.gs[0] - 1)) / 2
         self.h_adjust = (h_screen - self.ts*self.gs[1] - self.ms*(self.gs[1] - 1)) / 2
         
@@ -74,13 +83,15 @@ class SlidePuzzle:
         # Convert mouse position relative to tile position and check in grid
         if mouse[0]:
             tile = (mpos[0] - self.w_adjust)//self.ts , (mpos[1] - self.h_adjust)//self.ts
-            if self.is_grid(tile): 
+            if self.is_grid(tile) and self.finish == 0: 
                 if tile in self.adjacent():
                     self.switch(tile)
                     sound = pygame.mixer.Sound("./sounds/click.mp3")
                     pygame.mixer.Sound.play(sound)
-    
-    def draw(self, screen):
+            else: self.finish = 0
+            
+            
+    def draw(self, screen, clock):
         init_w, init_h = self.tilespos[(0, 0)][0] + self.w_adjust, self.tilespos[(0, 0)][1] + self.h_adjust
         init_w_size, init_h_size = self.ts*self.gs[0] + self.ms*(self.gs[0] - 1), self.ts*self.gs[1] + self.ms*(self.gs[1] - 1)
         
@@ -98,6 +109,22 @@ class SlidePuzzle:
         pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(goal_init_w-self.ms*2, goal_init_h-self.ms*2, goal_w+self.ms*4, goal_h+self.ms*4))
         screen.blit(pygame.transform.scale(self.pic, (goal_w, goal_h)), (goal_init_w, goal_init_h))
         
+        # Buttons
+        screen.blit(self.back_button, (self.w_screen-self.w/4-init_w*1.68,self.h_screen-self.h/4-15))
+        screen.blit(self.retry, (self.w_screen-self.w/4-init_w*0.98,self.h_screen-self.h/4-15))
+        
         # Winning condition
         if self.finish == 1:
-            screen.blit()
+            congrat = pygame.image.load('./ui_button/congrat.png')
+            congrat = pygame.transform.scale(congrat, (1829-250, 430-100))
+            # pygame.draw.rect(screen, (0, 0, 0, 0.2), pygame.Rect(0, 0, self.w_screen, self.h_screen))
+            s = pygame.Surface((self.w_screen, self.h_screen))  # the size of your rect
+            s.set_alpha(128)                # alpha level
+            s.fill((255,255,255))           # this fills the entire surface
+            screen.blit(s, (0,0)) 
+            screen.blit(congrat, (0, (self.h_screen-congrat.get_size()[1])/2))
+            
+            # clock.tick(10000)
+            # if pygame.time.wait(1000):
+            # self.finish = 0
+                
