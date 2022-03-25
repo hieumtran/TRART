3d61a71f067e7ec35ed092d76d6577a32f518acf
hieumtran <hieutran_2023@depauw.edu>
2022-03-25 17:51:40 -0400

    Update main file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import os, sys
-from slide_puzzle import SlidePuzzle
-from menu import menuUI
+from utils.slide_puzzle import SlidePuzzle
+from utils.menu import menuUI
 
 
 def main():
@@ -13,9 +13,9 @@ def main():
     
     w_screen, h_screen = pygame.display.get_surface().get_size() 
     
-    program = SlidePuzzle((3,3), 160, 5, w_screen, h_screen)
-    program1 = SlidePuzzle((4,4), 140, 5, w_screen, h_screen)
-    program2 = SlidePuzzle((5,5), 100, 5, w_screen, h_screen)
+    program_3x3 = SlidePuzzle((3,3), 160, 5, w_screen, h_screen)
+    program_4x4 = SlidePuzzle((4,4), 130, 5, w_screen, h_screen)
+    program_5x5 = SlidePuzzle((5,5), 100, 5, w_screen, h_screen)
     menu_ui = menuUI('./ui_button/DeSwipe.png', './ui_button/start.png', './ui_button/quit.png',
                      ['./ui_button/3x3.png', './ui_button/4x4.png', './ui_button/5x5.png'], './ui_button/back.png', w_screen, h_screen)
     
@@ -23,9 +23,8 @@ def main():
     cond = 0
     prev_cond = 0
     pos = w_screen, h_screen
+    win_cnt = 0
     while True:
-        dt = fpsclock.tick()/1000
-        
         # screen.fill((1,1,1)) # Filling with missing pictures
         screen.fill([255, 255, 255])
         
@@ -35,22 +34,20 @@ def main():
                 pos = w_screen, h_screen
                 prev_cond == cond
         elif cond >= 2:
-            program.draw(screen)
-            cond = program.update(pos, cond)
-            # elif cond == 2.5:
-            #     program = SlidePuzzle((3,3), 160, 5, w_screen, h_screen)
-            #     program.draw(screen)
-            #     cond -= 0.5
+            if cond == 2:
+                program_3x3.draw(screen, win_cnt)
+                cond, win_cnt = program_3x3.update(pos, cond, win_cnt)
+            elif cond == 3:
+                program_4x4.draw(screen, win_cnt)
+                cond, win_cnt = program_4x4.update(pos, cond, win_cnt)
+            elif cond == 4:
+                program_5x5.draw(screen, win_cnt)
+                cond, win_cnt = program_5x5.update(pos, cond, win_cnt)
         pygame.display.flip()
         
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    sys.exit()
-                if event.key == pygame.K_r:
-                    program = SlidePuzzle((3,3), 160, 5, w_screen, h_screen)
         
         if cond == -1:
             sys.exit()
